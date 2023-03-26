a78c1c534ededfa4488030753c03e0e34308d06d
Pavel-Medvedev <medvedev.pavel.al@outlook.com>
2023-03-26 09:51:24 -0700

    Выполнение лабораторной работы №2 по дисциплине Java программирование.

--- src/main/java/org/example/Lab1.java
+++ src/main/java/org/example/Lab1.java
@@ -25,9 +25,10 @@ public class Lab1 {
         return Math.sqrt(p * (p - a) * (p - b) * (p - c));
     }
     private static void printArea(Point3d point1, Point3d point2, Point3d point3) {
-        if (point1.equal(point2) || point1.equal(point3) || point2.equal(point3))
-            System.out.println("This points couldn\'t provide the triangle creation!");
+        double temp = Math.max(point3.distanceTo(point1), Math.max(point2.distanceTo(point3), point1.distanceTo(point2)));
+        if (point1.equal(point2) || point1.equal(point3) || point2.equal(point3) || (point1.distanceTo(point2) + point1.distanceTo(point3) + point2.distanceTo(point3) - temp) <= temp)
+            System.out.println("This points couldn't provide the triangle creation!");
         else
-            System.out.println(String.format("The area of triangle created by you\'r 3 points is: %f", computeArea(point1, point2, point3)));
+            System.out.println(String.format("The area of triangle created by you'r 3 points is: %.2f", computeArea(point1, point2, point3)));
     }
 }
--- src/main/java/org/example/Point3d.java
+++ src/main/java/org/example/Point3d.java
@@ -3,14 +3,11 @@ package org.example;
 public class Point3d extends Point2d {
     private double z;
     public Point3d(double x, double y, double z) {
-        super.setX(x);
-        super.setY(y);
+        super(x,y);
         this.z = z;
     }
     public Point3d() {
-        this.setX(0);
-        this.setY(0);
-        this.z = 0;
+        this(0,0,0);
     }
     public double getZ() {
         return z;
@@ -22,6 +19,6 @@ public class Point3d extends Point2d {
         return other.getX() == this.getX() && other.getY() == this.getY() && other.getZ() == this.z;
     }
     public double distanceTo(Point3d other) {
-        return Math.sqrt(Math.pow(Math.abs(other.getX() + this.getX()), 2) + Math.pow(Math.abs(other.getY() + this.getY()), 2) + Math.pow(Math.abs(other.getZ() + this.getZ()), 2));
+        return Math.sqrt(Math.pow(Math.abs(other.getX() - this.getX()), 2) + Math.pow(Math.abs(other.getY() - this.getY()), 2) + Math.pow(Math.abs(other.getZ() - this.getZ()), 2));
     }
 }
