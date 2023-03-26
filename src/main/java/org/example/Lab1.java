50f7c5058e6fa5f1c848e8ac06caa32b11b9f3e0
Pavel-Medvedev <medvedev.pavel.al@outlook.com>
2023-03-26 10:18:52 -0700

    Выполнение лабораторной работы №2 по дисциплине Java программирование.

--- src/main/java/org/example/Lab1.java
+++ src/main/java/org/example/Lab1.java
@@ -20,15 +20,19 @@ public class Lab1 {
         return sc.nextDouble();
     }
     private static double computeArea(Point3d point1, Point3d point2, Point3d point3) {
-        double a = point1.distanceTo(point2), b = point2.distanceTo(point3), c = point3.distanceTo(point1),
-                p = (a + b + c) / 2;
-        return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        double temp = Math.max(point3.distanceTo(point1), Math.max(point2.distanceTo(point3), point1.distanceTo(point2)));
+        if (point1.distanceTo(point2) + point1.distanceTo(point3) + point2.distanceTo(point3) - temp > temp) {
+            double a = point1.distanceTo(point2), b = point2.distanceTo(point3), c = point3.distanceTo(point1),
+                    p = (a + b + c) / 2;
+            return Math.sqrt(p * (p - a) * (p - b) * (p - c));
+        }
+        return 0;
     }
     private static void printArea(Point3d point1, Point3d point2, Point3d point3) {
-        double temp = Math.max(point3.distanceTo(point1), Math.max(point2.distanceTo(point3), point1.distanceTo(point2)));
-        if (point1.equal(point2) || point1.equal(point3) || point2.equal(point3) || (point1.distanceTo(point2) + point1.distanceTo(point3) + point2.distanceTo(point3) - temp) <= temp)
+        double result = computeArea(point1, point2, point3);
+        if (result == 0)
             System.out.println("This points couldn't provide the triangle creation!");
         else
-            System.out.println(String.format("The area of triangle created by you'r 3 points is: %.2f", computeArea(point1, point2, point3)));
+            System.out.println(String.format("The area of triangle created by you'r 3 points is: %.2f", result));
     }
 }
